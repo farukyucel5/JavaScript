a122c40805d7c91f0fb5b3b663b5ac61775c22aa
Faruk Yucel <farukyucel5@gmail.com>
2023-01-09 15:29:12 +0300

    javaScript Apps

---
+++ Lesson10/numberGuessing.js
@@ -0,0 +1,59 @@
+let answer = Math.floor(Math.random() * 10 + 1);
+let guesses = 0;
+
+document.getElementById("submitButton").onclick = function (){
+
+  let guess = document.getElementById("guessField").value
+  guesses+=1;
+  
+  if(guess == answer){
+    alert(`${answer} is the correct answer. It took you ${guesses} guesses`);
+    document.getElementById("submitButton")
+
+  }
+  else if(guess < answer){
+    alert("Too small!");
+  }
+  else{
+    alert("Too large!");
+  }
+}
+
+document.getElementById("resetBtn").onclick=function(){
+ 
+  window.location.reload()
+ 
+
+}
+
+
+document.getElementById("submitButton").onclick = function(){
+
+  let temp;
+
+  if(document.getElementById("cButton").checked){
+      temp = document.getElementById("textBox").value;
+      temp = Number(temp);
+      temp = toCelsius(temp);
+      document.getElementById("tempLabel").innerHTML = temp + "°C";
+  }
+  else if(document.getElementById("fButton").checked){
+      temp = document.getElementById("textBox").value;
+      temp = Number(temp);
+      temp = toFahrenheit(temp);
+      document.getElementById("tempLabel").innerHTML = temp + "°F";
+  }
+  else{
+      document.getElementById("tempLabel").innerHTML = "Select a unit";
+  }
+}
+
+function toCelsius(temp){
+  return (temp - 32) * (5/9);
+}
+
+function toFahrenheit(temp){
+  return temp * 9 / 5 + 32;
+}
+
+
