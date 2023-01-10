8d04b10fb536a9bf24b6aec52ceb27b3188592c1
Faruk Yucel <farukyucel5@gmail.com>
2023-01-10 17:07:27 +0300

    10.01.2023 Faruk

---
+++ Lesson11/2D_Arrays.js
@@ -0,0 +1,53 @@
+
+// 2D array = An array of arrays
+
+let fruits =           ["apples", "oranges", "bananas"];
+let vegetables = ["carrots", "onions", "potatoes"];
+let meats =         ["eggs", "chicken", "fish"];
+
+let groceryList = [fruits, vegetables, meats];
+
+groceryList[2][2] = "steak";
+
+for(let list of groceryList){
+    for(let food of list){
+        console.log(food);
+    }
+}
+
+// spread operator = allows an iterable such as an 
+// ...                             array or string to be expanded 
+//                                 in places where zero or more 
+//                                 arguments are expected
+//                                (unpacks the elements)
+
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+let maximum = Math.max(...numbers);
+console.log(maximum);
+
+
+let class1 = ["Spongebob", "Patrick", "Sandy"];
+let class2 = ["Squidward", "Mr.Krabs", "Plankton"];
+class1.push(...class2);
+console.log(...class1);
+console.log(class1);
+
+// rest parameters = represents an indefinite number
+// ...                             of parameters
+//                                (packs arguments into an array)
+
+let a = 1;
+let b = 2;
+let c = 3;
+let d = 4;
+let e = 5;
+
+console.log(sum(a, b, c, d, e));
+
+function sum(...numbers){
+    let total = 0;
+    for(let number of numbers){
+        total += number
+    }
+    return total
+}
